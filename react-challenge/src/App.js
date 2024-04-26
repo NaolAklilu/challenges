d1006fab4c24cfdf650b659be79493120c565e96
NaolAklilu <naolaklilu12@gmail.com>
2024-04-26 18:39:07 +0300

    add routing

--- react-challenge/src/App.js
+++ react-challenge/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ActorList from "./components/ActorList";
+import ActorDetail from "./components/ActorDetail";
+import PageNotFound from "./components/PageNotFound";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" Component={ActorList} />
+        <Route path="/actor/:id" Component={ActorDetail} />
+        <Route Component={PageNotFound} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
