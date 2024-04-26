365cea757b4837aa5899b28868f45ec4b0f7d405
NaolAklilu <naolaklilu12@gmail.com>
2024-04-26 18:42:24 +0300

    add unit test

--- react-challenge/src/App.test.js
+++ react-challenge/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Header from "./components/Header";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App Component", () => {
+  test("renders Header", () => {
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
+    const headerElement = screen.getByText(/Actors/i);
+    expect(headerElement).toBeInTheDocument();
+  });
 });
