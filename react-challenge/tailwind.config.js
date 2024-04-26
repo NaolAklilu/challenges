8e11d3b94f606205367b6bd85778043f8a0c6b7c
NaolAklilu <naolaklilu12@gmail.com>
2024-04-26 18:34:22 +0300

    add tailwind configuration

---
+++ react-challenge/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+export const purge = ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"];
+export const darkMode = false;
+export const theme = {
+  extend: {},
+};
+export const variants = {
+  extend: {},
+};
+export const plugins = [];
