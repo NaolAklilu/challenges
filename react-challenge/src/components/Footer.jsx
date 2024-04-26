b4f3d57a7b8281c32074f4c3f0c2bcf69df738a1
NaolAklilu <naolaklilu12@gmail.com>
2024-04-26 18:39:48 +0300

    add header, footer and page not found component

---
+++ react-challenge/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="w-full flex justify-center px-5 py-4 shadow-md = bg-white z-50 relative bottom-0">
+      <h1> Copyright &copy;2024</h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ react-challenge/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="w-full px-5 py-4 shadow-md fixed top-0 left-0 bg-white z-50">
+      <h1>Actors</h1>
+    </div>
+  );
+};
+
+export default Header;
